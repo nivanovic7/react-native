180ff5be3cd0929fca6ffafdc5b7c8b9fe82dc60
Nikola Ivanovic <ivanovic.nikola.ns@gmail.com>
2025-02-06 12:33:56 +0100

    added get messages request

--- redux/chatApi.js
+++ redux/chatApi.js
@@ -6,7 +6,10 @@ export const chatsApiSlice = apiSlice.injectEndpoints({
     getChats: builder.query({
       query: () => CHAT_URL,
     }),
+    getMessages: builder.query({
+      query: (id) => `${CHAT_URL}/${id}/message`,
+    }),
   }),
 });
 
-export const { useGetChatsQuery } = chatsApiSlice;
+export const { useGetChatsQuery, useGetMessagesQuery } = chatsApiSlice;
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,13 +1,32 @@
 import { useRoute } from "@react-navigation/native";
-import { Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { useGetMessagesQuery } from "../redux/chatApi";
 
 function ChatScreen({ route }) {
   const { id } = route.params;
-  return (
-    <View>
-      <Text>Chat id: {id}</Text>
-    </View>
-  );
+  const { data, isLoading } = useGetMessagesQuery(id);
+  if (data) console.log(data);
+
+  if (isLoading) return <ActivityIndicator size="small" />;
+
+  if (data) {
+    return (
+      <View>
+        {data.data.map((msg) => (
+          <Text key={msg._id}>
+            <Text style={styles.userName}> {msg.chatMessageUser.userName}</Text>
+            : {msg.chatMessageText}
+          </Text>
+        ))}
+      </View>
+    );
+  }
 }
 
 export default ChatScreen;
+
+const styles = StyleSheet.create({
+  userName: {
+    fontWeight: 600,
+  },
+});
